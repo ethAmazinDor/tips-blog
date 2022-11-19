f4ba20cef3c73ae836ca0d58b8572ac0cea4955f
Dorian Dubreus <doriandubreus@gmail.com>
2022-11-19 16:32:32 -0500

    setting up frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Dashboard from './pages/Dashboard'
+import Posts from './pages/Posts';
 function App() {
   return (
-    <>
-      hello
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='posts' element={<Posts />} />
+
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
